831a065765aa9390fa3b13968f6dcaada9d9caab
parsa13881 <mohammadpa87@gmail.com>
2016-03-29 12:07:50 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,26 +230,11 @@ function create_config( )
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
+💥Fire Bot 💥
+Developer : @parsaalemi
 
-https://github.com/SEEDTEAM/TeleSeed
-
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+thanks to : @rextoor
+---by parsa alemi
 ]],
     help_text_realm = [[
 Realm Commands:
